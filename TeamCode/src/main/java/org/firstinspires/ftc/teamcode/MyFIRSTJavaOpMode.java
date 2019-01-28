0f54a30692ecd886cb71477ec1fd45848a3203a1
NotScott <notscottlabs@gmail.com>
2019-01-28 17:03:06 -0500

    started work on mechanum wheel drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -14,14 +14,15 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     // Link to java docs https://ftctechnh.github.io/ftc_app/doc/javadoc/index.html
     
     // Sample mecanum drive code:
-    // https://ftcforum.usfirst.org/forum/ftc-technology/android-studio/6361-mecanum-wheels-drive-code-example
     // https://ftcforum.usfirst.org/forum/ftc-technology/android-studio/60054-mecanum-wheels-programming
     // https://github.com/trc492/FtcSamples/blob/master/Ftc3543Lib/src/main/java/trclib/TrcMecanumDriveBase.java
     // https://www.google.com/search?safe=on&q=ftc+java+mecanum
     
     private Gyroscope imu;
-    private DcMotor motorTest;
-    private DcMotor motorTest2;
+    private DcMotor motorTestLeftFront;
+    private DcMotor motorTestLeftRear;
+    private DcMotor motorTestRightFront;
+    private DcMotor motorTestRightRear;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
     private Servo servoTest;
@@ -30,10 +31,12 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     @Override
     public void runOpMode() {
         // What? No comments? What is this code supposed to do???
-
+        // Stuff.
         imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        motorTest2 = hardwareMap.get(DcMotor.class, "motorTest2");
+        motorTestLeftFront = hardwareMap.get(DcMotor.class, "leftfront");
+        motorTestLeftRear = hardwareMap.get(DcMotor.class, "leftback");
+        motorTestRightFront = hardwareMap.get(DcMotor.class, "rightfront");
+        motorTestRightRear = hardwareMap.get(DcMotor.class, "rightback");
         digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         servoTest = hardwareMap.get(Servo.class, "servoTest");
@@ -44,34 +47,60 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         waitForStart();
 
         // Run until the end of the match (driver presses STOP)
-        double tgtPower = 0;
-        double tgtPower2 = 0;
+        double tgtPowerRightFront = 0;
+        double tgtPowerRightRear = 0;
+        double tgtPowerLeftFront = 0;
+        double tgtPowerLeftRear = 0;
         while (opModeIsActive()) {
             /*CHANGES: Removed negative symbol from "tgtPower = this.gamepad1.left_stick_y;,
            Changed right and left for easier steering.
             Buttons added to do straight
              */
-            tgtPower = this.gamepad1.right_stick_y;
-            motorTest.setPower(tgtPower);
-            telemetry.addData("Target Power", tgtPower);
-            telemetry.addData("Motor Power", motorTest.getPower());
-            tgtPower2 = -this.gamepad1.left_stick_y;
-            motorTest2.setPower(tgtPower2);
-            //tgtPower2 *= -1;
-            telemetry.addData("Target Power 2", tgtPower2);
-            telemetry.addData("Motor Power 2", motorTest2.getPower());
-            //Press B, go forward.
-            while (this.gamepad1.b == true) {
-                motorTest.setPower(-1);
-                motorTest2.setPower(1);
+            tgtPowerRightFront = this.gamepad1.right_stick_y;
+            tgtPowerRightRear = this.gamepad1.right_stick_y;
+            motorTestRightFront.setPower(tgtPowerRightFront);
+            motorTestRightRear.setPower(tgtPowerRightRear);
+            telemetry.addData("Target Power Right Front", tgtPowerRightFront);
+            telemetry.addData("Target Power Right Rear", tgtPowerRightRear);
+            telemetry.addData("Motor Power Right Front", motorTestRightFront.getPower());
+            telemetry.addData("Motor Power Right Rear", motorTestRightRear.getPower());
+            tgtPowerLeftFront = -this.gamepad1.left_stick_y;
+            tgtPowerLeftRear = -this.gamepad1.left_stick_y;
+            motorTestLeftFront.setPower(tgtPowerLeftFront);
+            motorTestLeftRear.setPower(tgtPowerLeftRear);
+            telemetry.addData("Target Power Left Front", tgtPowerLeftFront);
+            telemetry.addData("Target Power Left Rear", tgtPowerLeftRear);
+            telemetry.addData("Motor Power Left Front", motorTestLeftFront.getPower());
+            telemetry.addData("Motor Power Left Rear", motorTestLeftRear.getPower());
+            //right trigger go forward.
+            while (this.gamepad1.right_trigger != 0) {
+                motorTestLeftFront.setPower(-1);
+                motorTestLeftRear.setPower(-1);
+                motorTestRightFront.setPower(1);
+                motorTestRightRear.setPower(1);
             }
-            while (this.gamepad1.a == true) {
-                motorTest.setPower(1);
-                motorTest2.setPower(-1);
+            //left trigger go backwards
+            while (this.gamepad1.left_trigger != 0) {
+                motorTestLeftFront.setPower(1);
+                motorTestLeftRear.setPower(1);
+                motorTestRightFront.setPower(-1);
+                motorTestRightRear.setPower(-1);
             }
             telemetry.addData("Status", "Running");
             telemetry.update();
+            // https://ftcforum.usfirst.org/forum/ftc-technology/android-studio/6361-mecanum-wheels-drive-code-example
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
 
+            motorTestLeftFront.setPower(v1);
+            motorTestRightFront.setPower(v2);
+            motorTestLeftRear.setPower(v3);
+            motorTestRightRear.setPower(v4);
         }
     }
 }
